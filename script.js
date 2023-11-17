42425f34ba8d340553f02c2cd60c57c558356395
SUBHRO MOHANTA <117219000+SubhroMohanta@users.noreply.github.com>
2023-11-18 00:13:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const  navbar = document.querySelector('.navbar');
+const menu = document.querySelector('#menu-icon');
+
+menu.onclick =()=>{
+        // menu.classList.toggle('bx-menu');
+        navbar.classList.toggle('open');
+    }
+    
+    // function toggleMenu(){
+    
+
+    //     menu.classList.toggle("active");
+    //     if(menu.classList.contains("open")){
+    //        navbar.style
+    //     }else{
+    //         console.log("Not Done")
+    //     }
+
+// }
